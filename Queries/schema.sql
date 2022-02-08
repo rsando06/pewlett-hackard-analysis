2538287aa56ec6fc1df508d6845191889d034c8d
Rafael Sandoval <rsando06@hotmail.com>
2022-02-07 23:08:55 -0800

    added sales and development queries.

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -99,7 +99,7 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 SELECT * FROM retirement_info;
 
 -- Joining departments and dept_manager tables
-SELECT ds.dept_name,
+SELECT d.dept_name,
      dm.emp_no,
      dm.from_date,
      dm.to_date
@@ -134,3 +134,66 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+SELECT ce.emp_no, ce.first_name, ce.last_name, d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- Sales Team query for retirees in their dept.
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    d.dept_name
+INTO sales_query
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+LEFT JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+-- Sales and Development Team query
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    d.dept_name
+INTO sales_and_development_query
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+LEFT JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
