8da1993d68b2b3417b1ef46ad240a9a07fd7143f
Rafael Sandoval <rsando06@hotmail.com>
2022-02-12 16:45:52 -0800

    added final .csv files.

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,46 @@
+-- Retrieve information from Employees and Titles tables into a new one and
+-- export into a new csv.
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no
+
+-- Use Dictinct with Order By to remove duplicate rows.
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+WHERE (rt.to_date = '9999-01-01')
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Retrieve the number of employees by their most recent job title who are 
+-- about to retire to find the number of retiring titles.
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY count DESC
+
+-- Retrieve info to create a table that hold the employees who are eligible
+-- to participate in a mentorship program.
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+e.first_name, 
+e.last_name, 
+e.birth_date, 
+de.from_date, 
+de.to_date, 
+t.title
+INTO mentorship_elegibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
